34acf0c20e2f269c0282fc196a0a5368a3584020
walt-er-dev <waltercarraz04@gmail.com>
2025-04-29 09:52:44 -0400

    create react app

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
 import express from "express";
+import "dotenv/config";
+import cors from "cors";
+
+const app = express();
+app.disable("x-powered-by");
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const PORT = process.env.PORT ?? 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port http://localhost:${PORT}`);
+});
